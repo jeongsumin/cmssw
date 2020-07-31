b81f5d40c675363470075917a95acb23add2843a
jeongsumin <yami4515@gmail.com>
2020-07-31 18:24:34 +0900

    fixed the case of hv status

--- DQM/RPCMonitorClient/src/RPCDcsInfoClient.cc
+++ DQM/RPCMonitorClient/src/RPCDcsInfoClient.cc
@@ -12,42 +12,11 @@ RPCDcsInfoClient::RPCDcsInfoClient(const edm::ParameterSet& ps) {
 RPCDcsInfoClient::~RPCDcsInfoClient() {}
 
 void RPCDcsInfoClient::beginJob() {}
-/*
-void RPCDcsInfoClient::dqmEndLuminosityBlock(DQMStore::IBooker& ibooker,
-                                             DQMStore::IGetter& igetter,
-                                             edm::LuminosityBlock const& l,
-                                             edm::EventSetup const& c) {
-  unsigned int nlumi = l.id().luminosityBlock();
 
-  if (nlumi + 1 > DCS.size())
-    DCS.resize(nlumi + 1);
-
-  MonitorElement* DCSbyLS_ = igetter.get(dcsinfofolder_ + "/DCSbyLS");
-
-  if (!DCSbyLS_)
-    return;
-
-  if (TH1F* h1 = DCSbyLS_->getTH1F()) {
-    int hvStatus = 0;
-
-    if (h1->GetBinContent(1) != 0) {
-      hvStatus = 0;  // set to 0 because HV was off (!)
-    } else {
-      hvStatus = 1;  // set to 1 because HV was on (!)
-    }
-
-    DCS[nlumi] = hvStatus;
-  }
-
-  return;
-}
-*/
 void RPCDcsInfoClient::dqmEndJob(DQMStore::IBooker& ibooker, DQMStore::IGetter& igetter) {
   // book
   ibooker.cd();
   ibooker.setCurrentFolder(dcsinfofolder_);
-
-  std::cout << dcsinfofolder_ << std::endl;
 
   unsigned int nlsmax = DCS.size();
   MonitorElement* reportSummaryMap_ = igetter.get(dqmprovinfofolder_ + "/reportSummaryMap");
@@ -60,28 +29,28 @@ void RPCDcsInfoClient::dqmEndJob(DQMStore::IBooker& ibooker, DQMStore::IGetter& 
   //reportSummaryMap histogram: DCS HV Status and Beam Status per Lumisection
   if (TH2F* h2 = reportSummaryMap_->getTH2F()) {
     nlsmax = h2->GetNbinsX();
-    int hvStatus = -1; // Initialized as -1 follow by reportSummaryMap
+    int hvStatus = 0;
     const char * label_name = "RPC";
     unsigned int rpc_num = 0;
     if (nlsmax + 1 > DCS.size())
       DCS.resize(nlsmax + 1);
 
     for (int ybin = 0; ybin < h2->GetNbinsY(); ++ybin) {
-      std::cout << h2->GetYaxis()->GetBinLabel(ybin+1) << std::endl;
       if (strcmp(h2->GetYaxis()->GetBinLabel(ybin+1), label_name) == 0)
         rpc_num = ybin + 1;  
     }
 
     for (unsigned int nlumi = 0; nlumi < nlsmax; ++nlumi) {
-      //reportSummaryMap bins are filled -1(initialized), 0(no dcs bit), or 1(dcs bit)
+      //reportSummaryMap bins are filled -1(initialized), 0(dcs bit = 0), or 1(dcs bit != 0)
+      //0 and 1: That LS has DCS status collection!
       int rpc_dcsbit = h2->GetBinContent(nlumi+1, rpc_num);
-      if (rpc_dcsbit == 0) {
-        hvStatus = 0;  // set to 0 because HV was off (!)
-      } else if (rpc_dcsbit == 1){
+
+      if (rpc_dcsbit != -1) {
         hvStatus = 1;  // set to 1 because HV was on (!)
-      } 
+      } else {
+        hvStatus = 0;  // set to 0 because HV was off (!)
+      }
       DCS[nlumi+1] = hvStatus;
-      hvStatus = -1;
     }
   }
 
@@ -93,8 +62,8 @@ void RPCDcsInfoClient::dqmEndJob(DQMStore::IBooker& ibooker, DQMStore::IGetter& 
   int lsCounter = 0;
   // fill
   for (unsigned int i = 0; i < nlsmax; i++) {
-    rpcHVStatus->setBinContent(i + 1, 1, DCS[i]);
-    lsCounter += DCS[i];
+    rpcHVStatus->setBinContent(i + 1, 1, DCS[i+1]);
+    lsCounter += DCS[i+1];
   }
 
   meName = dcsinfofolder_ + "/rpcHV";
